3a9101579a8a45697f61526a873102426106bdfb
Abdul Salim <100780536+salim1704@users.noreply.github.com>
2023-07-12 12:25:32 +0100

    added colors page- not working

--- src/pages/ColorsList/index.jsx
+++ src/pages/ColorsList/index.jsx
@@ -1,12 +1,34 @@
 import {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
+import colors from "../../../colors.json"
 
-function colors(){
+function ColorsList(){
     useEffect(() => {
         fetchColors()
       }, [])
-}
+
+
+const [colors, setColors] = useState([]);
 
 const fetchColors = async () => {
-    
+  const response = await fetch('colors.json');
+  const colors = await response.json()
+  setColors(colors.data)
 }
+
+return(
+  <div>
+      <h1>Color Page</h1>
+      <div id="colors">
+        {colors.map((el) => (
+          <p key={el.id}>
+            <Link to ={`/colors/${el.id}`}>{el.name}</Link>
+          </p>
+        ))}
+      </div>
+    </div>
+
+)
+}
+
+export default ColorsList
